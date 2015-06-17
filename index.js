fde4bd1f1a458168d398d42bfadd9c23f55d7cb0
mattfaluotico <matt.faluotico@gmail.com>
2015-06-16 23:26:04 -0700

    more your welcomes

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ fs.readFile(path, 'utf8', function(err, data){
   if (err)  throw err;
   var a = data.split('\n');
   var lines = a.length;
-
-  var i = Math.floor(Math.random() * lines);
+  
+  var i = Math.floor(Math.random() * (lines - 1));
   console.log(a[i]);
 });
