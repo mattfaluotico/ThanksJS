6d06b1b82ad278c91ccdaf723f0bac53f98046a1
mattfaluotico <matt.faluotico@gmail.com>
2015-06-16 23:08:35 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+#! /usr/bin/env node
+
+var fs = require('fs');
+var path = __dirname + "/.yourewelcome"
+
+fs.readFile(path, 'utf8', function(err, data){
+  if (err)  throw err;
+  var a = data.split('\n');
+  var lines = a.length;
+
+  var i = Math.floor(Math.random() * lines);
+  console.log(a[i]);
+});
